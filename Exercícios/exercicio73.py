48a26c7627add17989ae67f6f65b323bf6163815
anagpbritox <ana.giovanna@escolar.ifrn.edu.br>
2026-09-22 00:01:48 -0300

    Update exercicio73.py

--- Exercícios/exercicio73.py
+++ Exercícios/exercicio73.py
@@ -2,8 +2,9 @@
 # Crie uma tupla preenchida com os 20 primerios colocados da tabel a do Campeonato Brasileiro de Futebol, na ordem de colocação.
 # Depois mostre: A) Apenas os 5 primeiros colocados; B) Os últimos 4 colocados; C) Uma lista com os times em ordem alfabética; D) Em que posição está o time Chapeconense;
 
-times_campeonatobr = ('Flamengo', 'Palmeiras', 'Athletico-PR', 'Fluminense', 'Bahia', 'Cruzeiro', 'Coritiba', 'Atlético-MG', 'Red Bull Bragantino', 
-'Corinthians', 'São Paulo', 'Botafogo', 'Vitória', 'Santos', 'Grêmio', 'Mirassol', 'Vasco', 'Internacional', 'Remo', 'Chapecoense')
+times_campeonatobr = ('Flamengo', 'Palmeiras', 'Athletico-PR', 'Fluminense', 'Bahia', 
+'Cruzeiro', 'Coritiba', 'Atlético-MG', 'Red Bull Bragantino', 'Corinthians', 'São Paulo', 
+'Botafogo', 'Vitória', 'Santos', 'Grêmio', 'Mirassol', 'Vasco', 'Internacional', 'Remo', 'Chapecoense')
 
 print()
 print(f"Os 5 primeiros colocodos do Brasileirão: {times_campeonatobr[0:6]}", end='\n\n')
@@ -19,3 +20,4 @@ for pos, time in enumerate(times_campeonatobr):
     print(pos + 1, end='')
     print(' - ', end='') 
     print(time)
+print()
