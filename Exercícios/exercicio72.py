e701ec9d0db03728970ba0a5e2c0b7a9f6f2337b
anagpbritox <ana.giovanna@escolar.ifrn.edu.br>
2026-09-22 00:01:45 -0300

    Update exercicio72.py

--- Exercícios/exercicio72.py
+++ Exercícios/exercicio72.py
@@ -3,10 +3,11 @@
 # Seu programa deverá ler um número pelo teclado (entre 0 e 20) e mostrá-lo por extenso.
 
 print()
+
 num_extenso = ('zero', 'um', 'dois', 'três','quatro', 'cinco', 'seis', 
 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze','catorze', 
 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
 
 ler_numero = int(input("Digite um número entre 0 e 20: "))
 
-print(num_extenso[ler_numero])
+print(f"\n{num_extenso[ler_numero]}", end='\n\n')
